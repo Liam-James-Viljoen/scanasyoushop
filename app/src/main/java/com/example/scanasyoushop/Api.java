b9bd9e002dc812daa42dcf1b31b6759d0075b8a1
Liamj <liamjames2@hotmail.co.uk>
2019-03-10 23:14:52 +0000

    -Got the application working on wifi etc.

--- app/src/main/java/com/example/scanasyoushop/Api.java
+++ app/src/main/java/com/example/scanasyoushop/Api.java
@@ -3,7 +3,8 @@ package com.example.scanasyoushop;
 // REFRENCE ------------> https://www.simplifiedcoding.net/android-mysql-tutorial-to-perform-basic-crud-operation/
 
 public class Api {
-    private static final String ROOT_URL = "http://192.168.0.36/scanasyoushop/v1/api.php?apicall=";
+    private static final String ROOT_URL = "http://192.168.0.36:8080/scanasyoushop/v1/api.php?apicall=";
+
 
     public static final String URL_CREATE_USER = ROOT_URL + "createuser";
     public static final String URL_SELECT_USER = ROOT_URL + "selectuser";
--- app/src/main/java/com/example/scanasyoushop/MainMenu.java
+++ app/src/main/java/com/example/scanasyoushop/MainMenu.java
@@ -1,15 +1,88 @@
 package com.example.scanasyoushop;
 
-import android.content.Intent;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
 
 public class MainMenu extends AppCompatActivity {
+
+    private SurfaceView cameraView;
+    private TextView barcodeInfo;
+    private CameraSource cameraSource;
+
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+    }
+
+    public void startScanning(View view) {
+        cameraView = (SurfaceView) findViewById(R.id.cameraView);
+
+        BarcodeDetector detector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE).build();
+        cameraSource = new CameraSource.Builder(this, detector).setRequestedPreviewSize(640, 480).build();
+
+        if (!detector.isOperational()) {
+            Log.e("Detector Malfunction", "Detector Broke");
+            return;
+        }
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder holder) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+                    } else {
+                        try {
+                            cameraSource.start(cameraView.getHolder());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder holder) {
+
+                }
+
+                public void receiveDetections(Detector.Detections<Barcode> detections) {
+                    final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                    if (barcodes.size() != 0) {
+                        barcodeInfo.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                barcodeInfo.setText(barcodes.valueAt(0).displayValue);
+                                Log.i("Barcode Information", barcodeInfo.toString());
+                            }
+                        });
+                    }
+                }
+            });
     }
 }
