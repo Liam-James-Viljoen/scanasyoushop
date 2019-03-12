836d40848e582793e49df8f42a7f690555744617
Liamj <liamjames2@hotmail.co.uk>
2019-03-12 14:45:27 +0000

    -Third party barcode installed

--- app/src/main/java/com/example/scanasyoushop/MainMenu.java
+++ app/src/main/java/com/example/scanasyoushop/MainMenu.java
@@ -3,16 +3,21 @@ package com.example.scanasyoushop;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 
 public class MainMenu extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void scanbarcode (View view){
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this); // where this is activity
+        intentIntegrator.initiateScan(IntentIntegrator.ALL_CODE_TYPES); // or QR_CODE_TYPES if you need to scan QR
     }
 }
